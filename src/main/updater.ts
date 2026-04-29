c25eae91fbde9555b781efdddd539ebbf8fc1c22
MEHDImp4 <83414196+MEHDImp4@users.noreply.github.com>
2026-04-29 22:59:41 +0100

    feat: implement auto-updater functionality with periodic checks and restart prompts

--- src/main/updater.ts
+++ src/main/updater.ts
@@ -1,4 +1,5 @@
-import { autoUpdater } from 'electron-updater'
+import updater from 'electron-updater'
+const { autoUpdater } = updater
 import { dialog } from 'electron'
 import log from 'electron-log'
 
